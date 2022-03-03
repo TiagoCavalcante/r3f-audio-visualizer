2117bccca4dcf79da627dc5cf0e9045898b5d931
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-03-03 19:51:28 +0000

    Initialize bars array with 0s

--- dist/store.js
+++ dist/store.js
@@ -10,7 +10,7 @@ var _zustand = _interopRequireDefault(require("zustand"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const useStore = (0, _zustand.default)(() => ({
-  data: new Array(121)
+  data: new Array(121).fill(0)
 }));
 var _default = useStore;
 exports.default = _default;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import create from "zustand";
 
 const useStore = create(() => ({
-  data: new Array(121)
+  data: new Array(121).fill(0)
 }));
 
 export default useStore;
